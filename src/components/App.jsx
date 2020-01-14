5b3de65b01f2d5a6470218b1ab0f4e84e31593a3
ntompkins <ntompkins@classworks.com>
2020-01-14 16:52:16 -0600

    Add Router and other components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import 'isomorphic-fetch';
 import "es6-promise";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    let res = await fetch('https://jsonplaceholder.typicode.com/users');
+    let users = await res.json();
+    setUsers(users);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+
+    <Router>
+    <>
+      <p><Link to="/"><button className="btn btn-warning text-light">Preview All</button></Link></p>
+
+      <Switch>
+        <Route exact path="/" component={Preview} />
+        <Route path="/:id/details" component={Details} />
+      </Switch>
+    </>
+  </Router>
+
+
+  )
+
+}
